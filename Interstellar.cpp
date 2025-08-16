2f1ca5ce88e21d452772454146b3bd8613e866ec
Ky <kyngo992@gmail.com>
2025-08-15 23:43:39 -0600

    First commit

---
+++ Interstellar.cpp
@@ -0,0 +1,12 @@
+﻿// Interstellar.cpp : Defines the entry point for the application.
+//
+
+#include "Interstellar.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
